39e578170cb64a323039eaed701bbcdb166ad73b
Sorasuu <delamgithe@gmail.com>
2019-05-08 18:10:48 +0700

    add setting

---
+++ Setting/NewViewController.swift
@@ -0,0 +1,85 @@
+//
+//  NewViewController.swift
+//  Setting
+//
+//  Created by tang quang an on 5/7/19.
+//  Copyright © 2019 Setting. All rights reserved.
+//
+
+import UIKit
+import DLRadioButton
+
+class NewViewController: UIViewController {
+
+    @IBAction func didPressBack(_ sender: Any) {
+        let transition = CATransition()
+        transition.duration = 0.5
+        transition.type = CATransitionType.push
+        transition.subtype = CATransitionSubtype.fromLeft
+        transition.timingFunction = CAMediaTimingFunction(name:CAMediaTimingFunctionName.easeInEaseOut)
+        view.window!.layer.add(transition, forKey: kCATransition)
+        self.dismiss(animated: false) {
+        }
+        
+    }
+    
+    
+    @IBAction func didChangeDistanceMetrics(_ sender: DLRadioButton) {
+        if (sender.tag == 1) {
+            
+        } else {
+            
+        }
+    }
+    
+    
+    @IBAction func didChangeMarkersDisplay(_ sender: DLRadioButton) {
+        if (sender.tag == 1) {
+            
+        } else if (sender.tag == 2) {
+            
+        } else {
+            
+        }
+    }
+    
+    
+    @IBOutlet weak var btnMarkersDisplay: DLRadioButton!
+    
+    @IBOutlet weak var btnDistanceMetrics: DLRadioButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        btnDistanceMetrics.isMultipleSelectionEnabled = true
+        btnMarkersDisplay.isMultipleSelectionEnabled = true
+        
+        for button in btnDistanceMetrics.selectedButtons() {
+            
+            if (button.tag == 2) {
+                button.deselectOtherButtons()
+            }
+        }
+        btnDistanceMetrics.isMultipleSelectionEnabled = false
+        
+        for button in btnMarkersDisplay.selectedButtons() {
+            
+            if (button.tag == 3) {
+                button.deselectOtherButtons()
+            }
+        }
+        btnMarkersDisplay.isMultipleSelectionEnabled = false
+        
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Setting/ViewController.swift
+++ Setting/ViewController.swift
@@ -8,13 +8,125 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+@IBDesignable
+class DesignableView: UIView {
+}
 
+@IBDesignable
+class DesignableButton: UIButton {
+}
+
+@IBDesignable
+class DesignableLabel: UILabel {
+}
+
+@IBDesignable
+class DesignableTextField: UITextField {
+}
+
+extension UIView {
+    
+    @IBInspectable
+    var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+        }
+    }
+    
+    @IBInspectable
+    var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+    
+    @IBInspectable
+    var borderColor: UIColor? {
+        get {
+            if let color = layer.borderColor {
+                return UIColor(cgColor: color)
+            }
+            return nil
+        }
+        set {
+            if let color = newValue {
+                layer.borderColor = color.cgColor
+            } else {
+                layer.borderColor = nil
+            }
+        }
+    }
+    
+    @IBInspectable
+    var shadowRadius: CGFloat {
+        get {
+            return layer.shadowRadius
+        }
+        set {
+            layer.shadowRadius = newValue
+        }
+    }
+    
+    @IBInspectable
+    var shadowOpacity: Float {
+        get {
+            return layer.shadowOpacity
+        }
+        set {
+            layer.shadowOpacity = newValue
+        }
+    }
+    
+    @IBInspectable
+    var shadowOffset: CGSize {
+        get {
+            return layer.shadowOffset
+        }
+        set {
+            layer.shadowOffset = newValue
+        }
+    }
+    
+    @IBInspectable
+    var shadowColor: UIColor? {
+        get {
+            if let color = layer.shadowColor {
+                return UIColor(cgColor: color)
+            }
+            return nil
+        }
+        set {
+            if let color = newValue {
+                layer.shadowColor = color.cgColor
+            } else {
+                layer.shadowColor = nil
+            }
+        }
+    }
+}
+
+class ViewController: UIViewController, UIScrollViewDelegate {
+
+    @IBOutlet weak var settingScrollView: UIScrollView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let transition = CATransition()
+        transition.duration = 0.5
+        transition.type = CATransitionType.push
+        transition.subtype = CATransitionSubtype.fromRight
+        transition.timingFunction = CAMediaTimingFunction(name:CAMediaTimingFunctionName.easeInEaseOut)
+        view.window!.layer.add(transition, forKey: kCATransition)
+    }
 
 }
 
